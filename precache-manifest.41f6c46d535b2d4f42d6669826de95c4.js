5f7382fd11ace8d14c0858251c9b85c5e7a1a180
Pramod Gobburi <pramod.gobburi@gohpr.app>
2020-08-19 17:16:15 -0500

    Updates

---
+++ precache-manifest.41f6c46d535b2d4f42d6669826de95c4.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a6327954afbd37a29470db3a958c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee3eab237da25120555b",
+    "url": "/static/css/main.b6a47d1b.chunk.css"
+  },
+  {
+    "revision": "5a8c1222ee71be20715b",
+    "url": "/static/js/2.013e4b61.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.013e4b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3eab237da25120555b",
+    "url": "/static/js/main.72d80622.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/static/js/main.72d80622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745eb3fc7684dec469d3",
+    "url": "/static/js/runtime-main.5ac43070.js"
+  },
+  {
+    "revision": "b68d378fd740fa97b79d18f0a36745d2",
+    "url": "/static/media/android_cdn.b68d378f.png"
+  },
+  {
+    "revision": "fbda738bf07e532ab23db02b62f4cc35",
+    "url": "/static/media/aws_square.fbda738b.png"
+  },
+  {
+    "revision": "d70c756f2a739722a398819cfa7287a7",
+    "url": "/static/media/bitcoin.d70c756f.png"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "429a7d6234fa0577ddf32baaf2d15af0",
+    "url": "/static/media/cheep.429a7d62.png"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "4fd59ead57f5b7dfa3ca0db01414a256",
+    "url": "/static/media/etherum.4fd59ead.png"
+  },
+  {
+    "revision": "b5b9abbe2eb3c8ace76a194d2e43650d",
+    "url": "/static/media/favicon.b5b9abbe.png"
+  },
+  {
+    "revision": "7efb722b8e3baf41f94a566c43431e6b",
+    "url": "/static/media/freespeech_messenger.7efb722b.jpg"
+  },
+  {
+    "revision": "765361412c81b34b34b56d28f5aece00",
+    "url": "/static/media/fun_with_opencv.76536141.png"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "3e90170fe755f007db14c5b001625e4d",
+    "url": "/static/media/gophr_logo.3e90170f.png"
+  },
+  {
+    "revision": "e8c3a2184305e79b70209ad99f1e81d7",
+    "url": "/static/media/gophr_marketplace.e8c3a218.png"
+  },
+  {
+    "revision": "a38e2fabd3209d1daf59ff680460f0c7",
+    "url": "/static/media/in_store_kiosk.a38e2fab.png"
+  },
+  {
+    "revision": "e3125ffcd22e79155a4dcc35620724c4",
+    "url": "/static/media/invoice_manager.e3125ffc.png"
+  },
+  {
+    "revision": "c085e83e15808eff561f13f30ce937c2",
+    "url": "/static/media/landing-page.c085e83e.png"
+  },
+  {
+    "revision": "407ac8ec759dc018a534616cf4eb03c4",
+    "url": "/static/media/msu_logo.407ac8ec.png"
+  },
+  {
+    "revision": "92e7581ab14047afd5688bed0132b2c0",
+    "url": "/static/media/nucleo-logo.92e7581a.svg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "4fd5fcf6af4db678f179c460cd4f1970",
+    "url": "/static/media/pramod_logo.4fd5fcf6.png"
+  },
+  {
+    "revision": "8b30814216d63aaa0b633cea0ff0b56f",
+    "url": "/static/media/react_square.8b308142.png"
+  },
+  {
+    "revision": "98742a820d2986e78ae284d57deae457",
+    "url": "/static/media/ripp.98742a82.png"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/static/media/waves.11fa5f73.png"
+  }
+]);
